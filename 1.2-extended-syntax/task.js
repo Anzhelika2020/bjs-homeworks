962685cd3161a6be3f869c7032cd809e9760c697
Anzhelika Popova <anjelikasp@mail.ru>
2021-02-26 21:35:17 +0700

    task 1 done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,30 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+"use strict";
+
+function getResult(a, b, c) {
+	let A = a; 
+	let B = b; 
+	let C = c;
+	let D = (B ** 2) - (4 * A * C);
+
+	if (D < 0) {
+		return [];
+	} else if (D === 0) {
+		let x1 = (-B) / (2 * A);
+		return [x1];
+	} else if (D > 0) {
+		let x1 = (-B + Math.sqrt(D)) / (2 * A);
+		let x2 = (-B - Math.sqrt(D)) / (2 * A);
+		return [x1, x2];
+	}
 }
 
+
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+		// код для задачи №2 писать здесь
+		// return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+		// код для задачи №3 писать здесь
+		// return result;
 }
