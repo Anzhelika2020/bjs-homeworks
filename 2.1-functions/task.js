2fbd8e98c491e6c7d57aa3c1308f75e8cd9b27b9
Anzhelika Popova <anjelikasp@mail.ru>
2021-03-04 19:21:00 +0700

    task 2.1-2 done

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -47,3 +47,45 @@ function showSolutionsMessage(a, b, c) {
 
 console.log(showSolutionsMessage(7, 20, -3));
 
+// Задание 2
+
+function getAverageMark(marks) {
+	if (marks.length === 0) {
+		return 0;
+	} else {
+		let sumMarks = 0;
+		for (let i = 0; i < marks.length; i++) {
+			sumMarks += marks[i];
+		}
+		return sumMarks / marks.length;
+	};
+};
+
+function getAverageScore(data) {
+  let averageScore = {};
+  for (let subject in data) {
+    averageScore[subject] = getAverageMark(data[subject]);
+  };
+
+  let averageMarksArray = [];
+  for (let subject in averageScore) {
+    averageMarksArray.push(averageScore[subject])
+	}
+
+  averageScore.average = getAverageMark(averageMarksArray);
+
+  return averageScore;
+}
+
+let data = {
+  algebra: [2, 4, 5, 2, 3, 4],
+  geometry: [2, 4, 5],
+  russian: [3, 3, 4, 5],
+  phisics: [5, 5],
+  music: [2, 2, 6],
+  english: [4, 4, 3],
+  poetry: [5, 3, 4],
+  chemistry: [2],
+  french: [4, 4],
+};
+console.log(getAverageScore(data));
