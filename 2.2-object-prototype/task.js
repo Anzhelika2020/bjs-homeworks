9b646901e13ad04bdce4dd95aa123c64e95e77d8
Anzhelika Popova <anjelikasp@mail.ru>
2021-03-10 19:37:59 +0700

    task 2.2-1 done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,15 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict'
+
+//задача 1
+
+String.prototype.isPalindrome = function isPalindrome() {
+	let string = this.toLowerCase().replace(/\s+/g, "").split("");
+  return string.join("") === string.reverse().join("");
+}
+
+"А роза упала на лапу Азора".isPalindrome();
+
+//задача 2
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
