78edf0dd051eb4be01c926bfd942cf83254b4e85
Anzhelika Popova <anjelikasp@mail.ru>
2021-03-13 02:07:28 +0700

    Task 2.3 - 2 done partially

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -4,7 +4,7 @@
 
 class PrintEditionItem {
 	constructor(name, releaseDate, pagesCount) {
-		this.name = name;
+		this.name = String(name);
 		this.releaseDate = releaseDate;
 		this.pagesCount = pagesCount;
 		this.state = 100;
@@ -30,13 +30,14 @@ class PrintEditionItem {
 	};
 };
 
+/*
 const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
 
 console.log(sherlock.releaseDate); //2019
 console.log(sherlock.state); //100
 sherlock.fix();
 console.log(sherlock.state); //100
-
+*/
 
 class Magazine extends PrintEditionItem {
 	constructor (name, releaseDate, pagesCount) {
@@ -44,6 +45,7 @@ class Magazine extends PrintEditionItem {
 		this.type = "magazine";
 	};
 };
+
 
 class Book extends PrintEditionItem {
 	constructor (author, name, releaseDate, pagesCount) {
@@ -74,6 +76,7 @@ class DetectiveBook extends Book {
 	};
 };
 
+/*
 const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
 
 console.log(picknick.author); //"Аркадий и Борис Стругацкие"
@@ -81,9 +84,54 @@ picknick.state = 10;
 console.log(picknick.state); //10
 picknick.fix();
 console.log(picknick.state); //15
+*/
+
 
 // Задача 2
 
+class Library {
+	constructor(name) {
+		this.name = String(name);
+		this.books = [];
+
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		};	
+	}
+
+	findBookBy(type, value) {
+		let findBook;
+		for (let i = 0; i < this.books.length; i++) {
+			if (this.books[i][type] === value) {
+				findBook = this.books[i];
+				break;
+			} else {
+				findBook = null;
+			};
+		};
+		return findBook;
+	};
 
 
+	giveBookByName(bookName) {
 
+	};
+
+}
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
