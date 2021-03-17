486118c1aa4624a5fe5213aa6abc58d3ce25461b
Anzhelika Popova <anjelikasp@mail.ru>
2021-03-17 16:39:02 +0700

    task 3.1 - 2 done

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -21,22 +21,51 @@ function validateCount(value) {
   };
 };
 
+
+
 //Задача 2
 
 class Triangle {
-  conctructor(a, b, c) {
-    if (a + b < c || a + c < b || b + c < a) {
+  constructor(a, b, c) {
+
+    if (isNaN(a) || isNaN(b) || isNaN(c) || a <= 0 || b <= 0 || c <= 0 || (a + b) < c || (a + c) < b || (b + c) < a) {
       throw new Error ("Треугольник с такими сторонами не существует");
     }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
   };
 
   getPerimeter() {
-
+    return this.a + this.b + this.c;
   };
 
   getArea() {
+    let p = (1 / 2) * (this.a + this.b + this.c);
 
+    let area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return +area.toFixed(3);
   };
-
-
 };
+
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+
+  } catch { 
+    let err = "Ошибка! Треугольник не существует";
+
+    return {
+      getPerimeter() {
+        return err;
+      },
+
+      getArea() {
+      return err
+      }
+    };
+  };
+}
